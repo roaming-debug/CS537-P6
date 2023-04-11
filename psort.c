5ad70fc5ba3ac29ef91e3f12556ea5e8a009de94
Ezra Ge <ege2@wisc.edu>
2023-04-10 21:06:07 -0500

    initialize

---
+++ psort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void sort(void* start_add, void* end_add);
+
+int main(int argc, char** argv) {
+    char* input_name;
+    char* output_name;
+    int num_threads;
+
+    // while()
+}
