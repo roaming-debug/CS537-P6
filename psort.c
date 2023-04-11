e7001da8c0b234088d3b32c3b058a57fbd85a1a2
Matt <mjstilling@gmail.com>
2023-04-11 12:33:38 -0500

    Started main method

--- psort.c
+++ psort.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
+#include <fcntl.h>
+
+typedef struct {
+  int key;
+  char *pointer;
+} key_pointer;
 
 void sort(void* start_add, void* end_add);
 
 int main(int argc, char** argv) {
-    char* input_name;
-    char* output_name;
-    int num_threads;
+    if (argc != 4) {
+        fprintf(stderr, "%s", "Incorrect number of args\n");
+        exit(1);
+    }
+    char* input_name = argv[1];
+    char* output_name = argv[2];
+    int num_threads = atoi(argv[3]);
+
+    int input_fd;
+    if ((input_fd = open(input_name, O_RDONLY)) == -1) {
+        fprintf(stderr, "%s", "Error opening file\n");
+        exit(1);
+    }
+
+    char* input_map;
+    // input_map = mmap(), use fd above
+
+    // idea is to create a key_pointer struct for each record (after loading into memory with mmap) 
+    // since that is smaller in size and easier to sort with --> sort by key value
 
     // while()
+
+    close(input_fd);
 }
