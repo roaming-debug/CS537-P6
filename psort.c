69b1bc81954dcfd1b62a81d2396d740e4eace073
Ezra Ge <ege2@wisc.edu>
2023-04-11 22:58:13 -0500

    update: efficiently copy input file to output file using mmap and memcpy

--- psort.c
+++ psort.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
 #include <err.h>
 #include <errno.h>
@@ -9,38 +10,62 @@
 #include <unistd.h>
 #include <pthread.h>
 
-typedef struct chunk_info {
-    void* start_addr;
-    void* end_addr;
+typedef struct chunk_info
+{
+    void *start_addr;
+    void *end_addr;
 } chunk_info;
 
-void sort(chunk_info* info);
+void sort(chunk_info *info);
 
-int main(int argc, char** argv) {
-    char* input_name = argv[1];
+void sort(chunk_info *info)
+{
+    // printf("1");
+}
+
+int main(int argc, char **argv)
+{
+    char *input_name = argv[1];
     char* output_name = argv[2];
     int num_threads = atoi(argv[3]);
+
+    pthread_t *ps = malloc(sizeof(pthread_t) * num_threads);
     
-    pthread_t* ps = malloc(sizeof(pthread_t) * num_threads);
-    FILE* fp = fopen(input_name, "rw");
-    fseek(fp, 0, SEEK_END);
-    int file_size = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    int fd = fileno(fp);
-    void* start_mapp_addr = mmap(NULL, file_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
+    int in_fd = open(input_name, O_RDONLY);
+    struct stat in_stat;
+    struct stat out_stat;
+    fstat(in_fd, &in_stat);
+    int out_fd = open(output_name, O_CREAT|O_RDWR, in_stat.st_mode);
+    fstat(out_fd, &out_stat);
 
-    while(num_threads >= 1) {
-        void* start_add = start_mapp_addr;
-        int chunksize = file_size/num_threads;
-        for (int i = 0; i < num_threads; i++) {
-            chunk_info info;
-            info.start_addr = start_add;
-            info.end_addr = start_add+chunksize-1;
-            pthread_create(&ps[i], NULL, sort, &info);
-        }
+    ftruncate(out_fd, in_stat.st_size);
+    
+    void *in_mem = mmap(NULL, in_stat.st_size, PROT_READ, MAP_SHARED_VALIDATE, in_fd, 0);
+    void *out_mem = mmap(NULL, out_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED_VALIDATE, out_fd, 0);
+
+    memcpy(out_mem, in_mem, in_stat.st_size);
+
+    void *start_add = out_mem;
+    int chunksize = in_stat.st_size / num_threads;
+    for (int i = 0; i < num_threads; i++)
+    {
+        chunk_info info;
+        info.start_addr = start_add;
+        info.end_addr = start_add + chunksize - 1;
+        pthread_create(&ps[i], NULL, (void *(*)(void *))sort, &info);
     }
-
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_join(ps[i], NULL);
+    }
+    num_threads /= 2;
+    
+    
+    fsync(out_fd);
     free(ps);
-    fclose(fp);
+    munmap(out_mem, out_stat.st_size);
+    munmap(in_mem, in_stat.st_size);
+    close(in_fd);
+    close(out_fd);
     return 0;
 }
